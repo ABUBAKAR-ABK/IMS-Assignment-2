0fff4b439ac03a7f4bba2eda0c30bd50e3756b4f
aruns <arunsethu2007+git01@gmail.com>
2025-06-22 01:05:05 +0400

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '', // your MySQL password
+  password: 'user@SQL', // your MySQL password
   database: 'internconnect'
 });
 
